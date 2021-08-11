4003b53c7508b9a32e597291ddb762230fbdda83
aakashching <aakashching@gmail.com>
2021-08-11 19:28:52 +0530

    'v2'

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,2 +1,5 @@
-const baseUrl ="http://localhost:3000";
-module.exports=baseUrl;
+const baseUrl =
+  process.env.NODE_ENV !== "production"
+    ? "http://localhost:3000"
+    : "https://social-muzaffarpur.herokuapp.com";
+module.exports = baseUrl;
