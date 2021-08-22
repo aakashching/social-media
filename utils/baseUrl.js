0297f7e030043895cd92aac7647c1c088df61efc
aakashching <aakashching@gmail.com>
2021-08-22 19:58:49 +0530

    'fix'

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,5 +1,5 @@
 const baseUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
-    : "http://social-muzaffarpur.herokuapp.com";
+    : "https://social-muzaffarpur.herokuapp.com";
 module.exports = baseUrl;
