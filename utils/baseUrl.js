4617b47bd9ad2ee4acc326dddd7d767feb91e434
Aakash Kumar <aakash_k@hcl.com>
2022-12-13 02:20:04 +0530

    baseurl updated

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,5 +1,5 @@
 const baseUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
-    : "https://social-muzaffarpur.herokuapp.com";
+    : "https://social-media-production-a1ca.up.railway.app";
 module.exports = baseUrl;
