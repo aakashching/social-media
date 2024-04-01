8fe15a21de295d7ff5b6ec41bb4cca3ae31accc6
AKAS <80155432+aakashching@users.noreply.github.com>
2024-04-01 10:40:10 +0530

    Update baseUrl.js

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,5 +1,5 @@
 const baseUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
-    : "https://social-media-production-a1ca.up.railway.app";
+    : "https://social-media-7t1c.onrender.com";
 module.exports = baseUrl;
